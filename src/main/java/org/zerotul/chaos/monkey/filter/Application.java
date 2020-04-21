7b6bd0fc9eee6f32150ea51c635372d8d7574b4b
Konstantin Chipunov <manychip@gmail.com>
2020-04-21 16:51:46 +0800

    initial commit

---
+++ src/main/java/org/zerotul/chaos/monkey/filter/Application.java
@@ -0,0 +1,15 @@
+package org.zerotul.chaos.monkey.filter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
